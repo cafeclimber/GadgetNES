e22042941fbb3314d897b7f019d1ca9077b9f9fe
Ryan Campbell <rdcampbell1990@gmail.com>
2016-06-17 16:58:47 -0500

    Move header reading to main (correct?)

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,7 @@ fn main() {
     let rom_name = env::args().nth(1).unwrap();
 
     let cart_rom = read_cartridge(rom_name);
+    read_rom_header(&cart_rom);
     
     let mut nes = nes::Nes::new();
     nes.power_up(cart_rom);
@@ -23,4 +24,22 @@ fn read_cartridge<P: AsRef<Path>>(path: P) -> Vec<u8> {
     let mut file_buf = Vec::new();
     file.read_to_end(&mut file_buf).unwrap();
     file_buf
+}
+
+fn read_rom_header(cart_rom: &Vec<u8>) {
+    let header = ((cart_rom[0] as u32) << 16) | ((cart_rom[1] as u32) << 8) | (cart_rom[2] as u32); // TODO: Write better
+    assert_eq!(0x4e4553, header); // "NES"
+
+    let num_prg_rom_pages = cart_rom[3];
+    let num_chr_rom_pages = cart_rom[4];
+
+    let mapper = ((cart_rom[5] & 0b1111_0000) >> 4) | (cart_rom[6] & 0b1111_0000);
+    
+    let four_screen_mode = cart_rom[5] & (1 << 3);
+    let trainer_present = cart_rom[5] & (1 << 2);
+    let battery_backed = cart_rom[5] & (1 << 1);
+    let mirroring = cart_rom[5] & 1;
+
+    let playchoice = cart_rom[6] & (1 << 1); 
+    let unisystem = cart_rom[6] & 1;
 }
--- src/nes.rs
+++ src/nes.rs
@@ -2,9 +2,6 @@ use cpu::Cpu;
 use apu::Apu;
 use cart::Cartridge;
 
-// Constants for header file reading
-const NES: u32 = 0x4e4553;
-
 #[derive(Default)]
 pub struct Nes {
     cpu: Cpu,
@@ -24,20 +21,10 @@ impl Nes {
 
     pub fn power_up (&mut self, cart_rom: Vec<u8>) {
         self.cpu.power_up();
-        self.read_rom_header(&cart_rom);
         self.cart.load_cartridge(cart_rom);
 
         println!("{:#?}\n", self.cpu);
         println!("{:#?}", self.apu);
     }
 
-    fn read_rom_header(&mut self, cart_rom: &Vec<u8>) {
-        // TODO: Implement more reading of header formats
-        let header = ((cart_rom[0] as u32) << 16) | ((cart_rom[1] as u32) << 8) | (cart_rom[2] as u32);
-
-        match header {
-            NES => println!("Read iNES file format"),
-            _ => panic!("Unrecognized file type"),
-        }
-    }
 }
