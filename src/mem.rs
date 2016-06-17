ec7a3f5ff0e444d25c154b17fd2c6f2aaa5be2e4
Ryan Campbell <rdcampbell1990@gmail.com>
2016-06-17 12:57:28 -0500

    Start mem_map function

--- src/mem.rs
+++ src/mem.rs
@@ -23,7 +23,13 @@ const PPU_MIRRORS_BEG: u16 = 0x2008;
 const PPU_MIRRORS_SIZE: u16 = 0x1ff8;
 const PPU_MIRRORS_END: u16 = 0x3fff;
 
-const APU_REGS_BEG: u16 = 
+const APU_REGS_BEG: u16 = 0x4000;
+const APU_REGS_SIZE: u16 = 0x0020;
+const APU_REGS_END: u16 = 0x401f;
+
+const CARTRIDGE_SPACE_BEG: u16 = 0x4020;
+const CARTRIDGE_SPACE_SIZE: u16 = 0xBFE0;
+const CARTRIDGE_SPACE_END: u16 = 0xffff;
 
 pub struct Memory {
     ram: Box<[u8]>,
@@ -41,15 +47,17 @@ impl Memory {
         }
     }
 
-    pub fn read_byte(address: PhysAddr) -> u8 {
-
+    pub fn read_word(address: PhysAddr) -> u8 {
     }
 
-    pub fn write_byte() {}
+    pub fn write_word() {}
 }
 
 fn map_addr(virt_addr: u16) -> PhysAddr {
     match virt_addr{
-        
+        _ => panic!("Unrecognized Physical Address {:#x}", virt_addr),
     }
+}
+
+pub enum PhysAddr {
 }
